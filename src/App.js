ca95b49a4ae4fb9d60569c9b9ae5b7d2aa26ab95
Alex Basystyi <alexbasisty@gmail.com>
2020-06-25 15:09:00 +0200

    base file structure, sass node package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import MachineStatus from './Machine_stat';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -7,6 +9,7 @@ function App() {
     <>
     <h1>Hello!</h1>
     <MachineStatus />
+    <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p className="footer__text"><strong>Alex Basystyi</strong> assignments</p>
+        </footer>
+    )
+};
+
+export default Footer;
